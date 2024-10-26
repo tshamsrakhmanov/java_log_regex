9e23d98f502a962dcc49706a723c857f4e87391b
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-10-26 18:12:45 +0400

    changes in help

--- SampleLogger.java
+++ SampleLogger.java
@@ -18,11 +18,21 @@ public class SampleLogger {
     }
 
     public void help(){
-        System.out.println("// JAVA_LOG_REGEX [ HELP ]");
-        System.out.println("// bla");
-        System.out.println("// bla");
-        System.out.println("// bla");
-        System.out.println("// bla");
+        System.out.println("JAVA_LOG_REGEX [ HELP ]");
+        System.out.println("Usage: java -jar [REGEX] [PATH] [FILE NAME]");
+        System.out.println("[REGEX]{mandatory}");
+        System.out.println("    Regular expression by which each string of log file(s) will be matched");
+        System.out.println("    Matching process performed based on partly-matches, not a full-string");
+        System.out.println("    Correct regex must be provide");
+        System.out.println("[PATH]{mandatory}");
+        System.out.println("    Path to a folder, where log files reside");
+        System.out.println("    In case of missing .log files - program will exit with no result");
+        System.out.println("[FILE NAME]");
+        System.out.println("    Name of a file - with or without extention based on your desires - where to save regex match results");
+        System.out.println("    Multiple runs of the program with the same input - .log files - will lead to fill of the same file with same match results (append mode)");
+        System.out.println();
+        System.out.println("Author: Timur Shamsrakhmanov");
+        System.out.println("Autumn 2024");
     }
 
     public void error_no_files_detected(){
